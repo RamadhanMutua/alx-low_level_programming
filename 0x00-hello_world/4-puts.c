037e8f1f97c25bc18ff7330fd392c8c6c8238a67
RamadhanMutua <ramahassan01@gmail.com>
2023-02-16 11:53:39 -0800

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Description: File to print simple stuff
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
